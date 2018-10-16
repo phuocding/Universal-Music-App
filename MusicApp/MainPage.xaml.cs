84fba01c82995f0b3174417f3ec0859b1e434184
phuocding <phuocding@gmail.com>
2018-10-16 20:57:15 +0700

    create sugn up form

---
+++ MusicApp/MainPage.xaml.cs
@@ -0,0 +1,171 @@
+﻿using MusicApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MusicApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Entity.Member currentMember;
+        private static StorageFile file;
+        private static string UploadUrl;
+        public MainPage()
+        {
+            GetUploadUrl();
+            this.currentMember = new Entity.Member();
+            this.InitializeComponent();
+        }
+
+        private async void Handle_Signup(object sender, RoutedEventArgs e)
+        {
+            // do validate first.
+            this.currentMember.firstName = this.FirstName.Text;
+            this.currentMember.lastName = this.LastName.Text;
+            this.currentMember.email = this.Email.Text;
+            this.currentMember.password = this.Password.Password.ToString();
+            this.currentMember.avatar = this.ImageUrl.Text;
+            this.currentMember.phone = this.Phone.Text;
+            this.currentMember.address = this.Address.Text;
+            this.currentMember.introduction = this.Introduction.Text;
+            await ApiHandle.Sign_Up(this.currentMember);
+            Debug.WriteLine("Action success.");
+        }
+
+        private async void Capture_Photo(object sender, RoutedEventArgs e)
+        {
+            CameraCaptureUI captureUI = new CameraCaptureUI();
+            captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+            captureUI.PhotoSettings.CroppedSizeInPixels = new Size(200, 200);
+            file = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            if (file == null)
+            {
+                // User cancelled photo capture
+                return;
+            }
+            HttpUploadFile(UploadUrl, "myFile", "image/png");
+        }
+
+        private static async void GetUploadUrl()
+        {
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+            Uri requestUri = new Uri("https://1-dot-backup-server-002.appspot.com/get-upload-token");
+            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+            string httpResponseBody = "";
+            try
+            {
+                httpResponse = await httpClient.GetAsync(requestUri);
+                httpResponse.EnsureSuccessStatusCode();
+                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
+            }
+            Debug.WriteLine(httpResponseBody);
+            UploadUrl = httpResponseBody;
+        }
+
+        public async void HttpUploadFile(string url, string paramName, string contentType)
+        {
+            string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+            byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+
+            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
+            wr.ContentType = "multipart/form-data; boundary=" + boundary;
+            wr.Method = "POST";
+
+            Stream rs = await wr.GetRequestStreamAsync();
+            rs.Write(boundarybytes, 0, boundarybytes.Length);
+
+            string header = string.Format("Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n", paramName, "path_file", contentType);
+            byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(header);
+            rs.Write(headerbytes, 0, headerbytes.Length);
+
+            // write file.
+            Stream fileStream = await file.OpenStreamForReadAsync();
+            byte[] buffer = new byte[4096];
+            int bytesRead = 0;
+            while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                rs.Write(buffer, 0, bytesRead);
+            }
+
+            byte[] trailer = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+            rs.Write(trailer, 0, trailer.Length);
+
+            WebResponse wresp = null;
+            try
+            {
+                wresp = await wr.GetResponseAsync();
+                Stream stream2 = wresp.GetResponseStream();
+                StreamReader reader2 = new StreamReader(stream2);
+                //Debug.WriteLine(string.Format("File uploaded, server response is: @{0}@", reader2.ReadToEnd()));
+                //string imgUrl = reader2.ReadToEnd();
+                Uri u = new Uri(reader2.ReadToEnd(), UriKind.Absolute);
+                Debug.WriteLine(u.AbsoluteUri);
+                ImageUrl.Text = u.AbsoluteUri;
+                MyAvatar.Source = new BitmapImage(u);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error uploading file", ex.StackTrace);
+                Debug.WriteLine("Error uploading file", ex.InnerException);
+                if (wresp != null)
+                {
+                    wresp = null;
+                }
+            }
+            finally
+            {
+                wr = null;
+            }
+        }
+
+        private void Select_Gender(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioGender = sender as RadioButton;
+            this.currentMember.gender = Int32.Parse(radioGender.Tag.ToString());
+            Debug.WriteLine(this.currentMember.gender);
+        }
+
+        private void Change_Birthday(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args)
+        {
+            this.currentMember.birthday = sender.Date.Value.ToString("yyyy-MM-dd");
+        }
+
+        private async void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Basic X3a2ysfsOPIQrm7YnzzGextV9LFStCfEVcqylA6ucNRFRJUtlMOvk8CFdxS340YM");
+
+            //var content = new StringContent(JsonConvert.SerializeObject(member), System.Text.Encoding.UTF8, "application/json");
+            var response = httpClient.GetAsync("https://2-dot-backup-server-002.appspot.com/_api/v2/members/information");
+            var contents = await response.Result.Content.ReadAsStringAsync();
+            Debug.WriteLine(contents);
+        }
+
+    }
+}
---
+++ MusicApp/Services/ApiHandle.cs
@@ -0,0 +1,40 @@
+﻿using MusicApp.Entity;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.Services
+{
+    class ApiHandle
+    {
+        public static String API_REGISTER = "https://2-dot-backup-server-002.appspot.com/_api/v2/members";
+        private static String API_LOGIN = "https://2-dot-backup-server-002.appspot.com/_api/v2/members/authentication";
+        public static String API_CREATE_SONG = "http://2-dot-backup-server-002.appspot.com/_api/v2/songs";
+        public static String API_MY_SONG = "http://2-dot-backup-server-002.appspot.com/_api/v2/songs/get-mine";
+
+        public async static Task<string> Sign_Up(Member member)
+        {
+            HttpClient httpClient = new HttpClient();
+            var content = new StringContent(JsonConvert.SerializeObject(member), System.Text.Encoding.UTF8, "application/json");
+            var response = httpClient.PostAsync(API_REGISTER, content);
+            var contents = await response.Result.Content.ReadAsStringAsync();
+            Debug.WriteLine(contents);
+            return contents;
+        }
+
+        public async static Task<string> Create_Song(Song song)
+        {
+            HttpClient httpClient = new HttpClient();
+            var content = new StringContent(JsonConvert.SerializeObject(song), System.Text.Encoding.UTF8, "application/json");
+            var response = httpClient.PostAsync(API_CREATE_SONG, content);
+            var contents = await response.Result.Content.ReadAsStringAsync();
+            Debug.WriteLine(contents);
+            return contents;
+        }
+    }
+}
